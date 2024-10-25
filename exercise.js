76c147c2d39d5b441a55a4f982bbcc622561f149
Yumil Rueda Flores <yumil.ruedaflores@abim.tech>
2024-10-25 09:25:18 +0200

    ex l

---
+++ exercise.js
@@ -0,0 +1,26 @@
+/* *Instructions*
+    You will solve one of the most famous cypher... Caesar's cypher.
+
+    This is a shift cypher meaning that all the letters shift it's position by a set amount.
+
+    You need to implement a cypher that rotates each later exactly 13 positions (using only the English alphabeth),
+    as input you will get a coded string and you must return the decoded result.
+
+
+*/
+
+/** DO NOT CHANGE THE FUNCTION NAME **/
+const cypher = (encoded) => {
+    
+    /* Only make changes below this comment */
+    
+   return encoded;
+    /* Only make changes above this comment */
+}
+
+
+
+
+
+/** DO NOT CHANGE THE LINE BELOW **/
+module.exports.cypher = cypher;
